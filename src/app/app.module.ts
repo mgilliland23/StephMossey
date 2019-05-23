2bb8444d1b389d9e2a79e36e016e7e689fbd5973
Matt Gilliland <mgilliland23@gmail.com>
2019-05-22 20:16:09 -0400

    created inital site files with mdbootstrap pro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModulesPro.forRoot()
   ],
-  providers: [],
+  providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
